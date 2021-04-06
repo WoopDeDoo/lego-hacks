14ced877fa816258c10a4280ed08e80c516c2e0c
swat turret <swatturret@protonmail.ch>
2021-04-06 15:46:23 +0100

    bypass grillies diner anticheat

---
+++ grillies anticheat bypass.lua
@@ -0,0 +1,16 @@
+local RS = game:GetService("ReplicatedStorage")
+local players = game:GetService("Players")
+local lp = players.LocalPlayer
+local gmt = getrawmetatable(game)
+local oldNameCall = gmt.__namecall
+
+setreadonly(gmt, false)
+
+gmt.__namecall = newcclosure(function(Self, ...)
+    local args = {...}
+    local namecallMethod = getnamecallmethod()
+    if not checkcaller() and Self == game.ReplicatedStorage.Remotes.Anti and namecallMethod == "FireServer" then
+        return nil
+    end
+    return oldNameCall(Self, ...)
+end)
