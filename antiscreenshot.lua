2dc9e30c842d6ee100c5272484bd0ad738903a71
swat turret <swatturret@protonmail.ch>
2021-04-08 21:50:45 +0100

    antiscreenshot.lua

---
+++ antiscreenshot.lua
@@ -0,0 +1,9 @@
+--[[
+    !!!!!!!!!!!!!!! IMPORTANT !!!!!!!!!!!!!
+    put this script in ur exploits autoexec folder
+    because roblox added a thing where when u get reported
+    it screenshots ur roblox window and sends it to
+    a moderator
+--]]
+setfflag("AbuseReportScreenshotPercentage", 0)
+setfflag("AbuseReportScreenshot", "False")
