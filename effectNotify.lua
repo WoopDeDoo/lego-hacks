bd140daaf57a9b01745769ab117f524d33f09f4d
swatTurret <69762985+swatTurret@users.noreply.github.com>
2021-04-10 16:18:00 +0100

    effectNotify
    effectNotify - a notification system for horrific housing
    syntax: effectNotify(text,emoji,textScaled,length)
    the length argument is optional, if no length is specified it defaults to 5 seconds
    game link: https://web.roblox.com/games/263761432/Horrific-Housing

---
+++ effectNotify.lua
@@ -0,0 +1,20 @@
+-- vars
+local rs = game:GetService("ReplicatedStorage")
+local plrs = game:GetService("Players")
+local lp = plrs.LocalPlayer
+local pgui = lp.PlayerGui
+-- function
+function effectNotify(text, emoji, textScaled, length)
+	if typeof(text) ~= "string" then error("argument text needs to be string, instead got "..typeof(text)) return end
+	if typeof(emoji) ~= "string" then error("argument emoji needs to be string, instead got "..typeof(emoji)) return end
+	if typeof(textScaled) ~= "boolean" then error("argument textScaled needs to be bool, instead got "..typeof(textScaled)) return end
+	if typeof(length) ~= "number" then warn("type of argument length needs to be number, resulting to default number of 5") length = 5 end
+	print(typeof(text).." "..typeof(emoji).." "..typeof(textScaled).." "..typeof(length))
+	local effect = rs.EffectsGui.Burning:Clone()
+	effect.Parent = pgui.Effects.StatList
+	effect.Title.Text = text
+	effect.Title.TextScaled = textScaled
+	effect.TextIcon.Text = emoji
+	wait(5)
+	effect:Destroy()
+end
