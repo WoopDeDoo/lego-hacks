dcfe382e2d2c05c342036f909cd0977ef43dc219
swatTurret <69762985+swatTurret@users.noreply.github.com>
2021-04-10 17:50:25 +0100

    Update effectNotify.lua

--- effectNotify.lua
+++ effectNotify.lua
@@ -1,4 +1,3 @@
--- vars
 local rs = game:GetService("ReplicatedStorage")
 local plrs = game:GetService("Players")
 local lp = plrs.LocalPlayer
@@ -10,11 +9,31 @@ function effectNotify(text, emoji, textScaled, length)
 	if typeof(textScaled) ~= "boolean" then error("argument textScaled needs to be bool, instead got "..typeof(textScaled)) return end
 	if typeof(length) ~= "number" then warn("type of argument length needs to be number, resulting to default number of 5") length = 5 end
 	print(typeof(text).." "..typeof(emoji).." "..typeof(textScaled).." "..typeof(length))
-	local effect = rs.EffectsGui.Burning:Clone()
-	effect.Parent = pgui.Effects.StatList
-	effect.Title.Text = text
-	effect.Title.TextScaled = textScaled
-	effect.TextIcon.Text = emoji
-	wait(5)
-	effect:Destroy()
+	local notifications = {}
+	for _, v in pairs(pgui.Effects.StatList:GetChildren()) do
+		if v.Name == "notification" then
+			table.insert(notifications, v)
+		end
+	end
+	if notifications[1] then
+		if not notifications[1].Title.Text == text then
+			local effect = rs.EffectsGui.Burning:Clone()
+			effect.Name = "notification"
+			effect.Parent = pgui.Effects.StatList
+			effect.Title.Text = text
+			effect.Title.TextScaled = textScaled
+			effect.TextIcon.Text = emoji
+			wait(length)
+			effect:Destroy()
+		end
+	else
+		local effect = rs.EffectsGui.Burning:Clone()
+		effect.Name = "notification"
+		effect.Parent = pgui.Effects.StatList
+		effect.Title.Text = text
+		effect.Title.TextScaled = textScaled
+		effect.TextIcon.Text = emoji
+		wait(length)
+		effect:Destroy()
+	end
 end
