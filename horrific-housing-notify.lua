bdd4006d9df2440252d21fb9fe9212a43cdbd21d
swat turret <swatturret@protonmail.ch>
2021-04-11 00:12:13 +0100

    gamemode detection

--- horrific-housing-notify.lua
+++ horrific-housing-notify.lua
@@ -1,8 +1,23 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/swatTurret/lego-hacks/main/effectNotify.lua'))()
 local plrs = game:GetService("Players")
 local lp = plrs.LocalPlayer
-lp.PlayerGui.Message.TextLabel.Changed:Connect(function()
-	if lp.PlayerGui.Message.TextLabel.Text ~= "" then
+local pgui = lp.PlayerGui
+pgui.Message.TextLabel.Changed:Connect(function()
+	if pgui.Message.TextLabel.Text ~= "" then
 		effectNotify(lp.PlayerGui.Message.TextLabel.Text, "⚡", true, 5)
 	end
 end)
+local gamemode = pgui.Message.TextLabel:Clone()
+pgui.Message.TextLabel.TextTransparency = 1
+gamemode.Name = "gamemode"
+gamemode.Parent = pgui.Message
+gamemode.BackgroundTransparency = 1
+gamemode.TextColor3 = Color3.new(255, 255, 255)
+gamemode.Name = "gamemode"
+gamemode.Text = "Current game mode: "..pgui:WaitForChild("GameModeInfo").Frame.Frame2.Title.Text.."."
+pgui:WaitForChild("GameModeInfo").Frame.Frame2.Title.Changed:Connect(function()
+	if pgui.GameModeInfo.Frame.Frame2.Title.Text ~= "" then
+		gamemode.Text = "Current game mode: "..pgui.GameModeInfo.Frame.Frame2.Title.Text
+	end
+end)
+pgui.Message.TextLabel.TextTransparency = 1
