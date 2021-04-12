3cc918e2726bd15337c842bcd4aa01b246330e4a
swatTurret <swatturret@protonmail.ch>
2021-04-12 04:39:00 +0100

    gay

--- effectNotify.lua
+++ effectNotify.lua
@@ -4,20 +4,31 @@ local plrs = game:GetService("Players")
 local lp = plrs.LocalPlayer
 local pgui = lp.PlayerGui
 -- function
-function effectNotify(text, emoji, textScaled, length)
-	if typeof(text) ~= "string" then error("argument text needs to be string, instead got "..typeof(text)) return end
-	if typeof(emoji) ~= "string" then error("argument emoji needs to be string, instead got "..typeof(emoji)) return end
-	if typeof(textScaled) ~= "boolean" then error("argument textScaled needs to be bool, instead got "..typeof(textScaled)) return end
-	if typeof(length) ~= "number" then warn("type of argument length needs to be number, resulting to default number of 5") length = 5 end
-	print(typeof(text).." "..typeof(emoji).." "..typeof(textScaled).." "..typeof(length))
-	local notifications = {}
-	for _, v in pairs(pgui.Effects.StatList:GetChildren()) do
-		if v.Name == "notification" then
-			table.insert(notifications, v)
+if game.GameId == 263761432 then
+	function effectNotify(text, emoji, textScaled, length)
+		if typeof(text) ~= "string" then error("argument text needs to be string, instead got "..typeof(text)) return end
+		if typeof(emoji) ~= "string" then error("argument emoji needs to be string, instead got "..typeof(emoji)) return end
+		if typeof(textScaled) ~= "boolean" then error("argument textScaled needs to be bool, instead got "..typeof(textScaled)) return end
+		if typeof(length) ~= "number" then warn("type of argument length needs to be number, resulting to default number of 5") length = 5 end
+		print(typeof(text).." "..typeof(emoji).." "..typeof(textScaled).." "..typeof(length))
+		local notifications = {}
+		for _, v in pairs(pgui.Effects.StatList:GetChildren()) do
+			if v.Name == "notification" then
+				table.insert(notifications, v)
+			end
 		end
-	end
-	if notifications[1] then
-		if notifications[1].Title.Text ~= text then
+		if notifications[1] then
+			if notifications[1].Title.Text ~= text then
+				local effect = rs.EffectsGui.Burning:Clone()
+				effect.Name = "notification"
+				effect.Parent = pgui.Effects.StatList
+				effect.Title.Text = text
+				effect.Title.TextScaled = textScaled
+				effect.TextIcon.Text = emoji
+				wait(length)
+				effect:Destroy()
+			end
+		else
 			local effect = rs.EffectsGui.Burning:Clone()
 			effect.Name = "notification"
 			effect.Parent = pgui.Effects.StatList
@@ -27,14 +38,7 @@ function effectNotify(text, emoji, textScaled, length)
 			wait(length)
 			effect:Destroy()
 		end
-	else
-		local effect = rs.EffectsGui.Burning:Clone()
-		effect.Name = "notification"
-		effect.Parent = pgui.Effects.StatList
-		effect.Title.Text = text
-		effect.Title.TextScaled = textScaled
-		effect.TextIcon.Text = emoji
-		wait(length)
-		effect:Destroy()
 	end
+else
+	lp:kick("use the correct game fucktard")
 end
--- silent aim.lua
+++ silent aim.lua
@@ -1,3 +1,6 @@
+if game.GameId ~= 286090429 then
+	game.Players.LocalPlayer:kick("use the correct game fucktard")
+end
 -- Gui to Lua
 -- Version: 3.2
 
